6334ed484ce56b83a4adf4c0b92a1cc90afa7fa3
haskev <kevin@hardanimeshirts.com>
2025-10-28 18:05:21 -0700

    Added fonts

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="font-annexxus">
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+<div class="text-3xl tracking-normal">hardanimeshirts.com</div>
+<p class="text-base tracking-wide">
+	Visit <a class="font-bold" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 </p>
